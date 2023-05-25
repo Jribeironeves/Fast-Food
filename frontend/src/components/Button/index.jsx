a593908538c311bc6c5076417aa1c029e919bb08
Jesiel Neves <rjesiel13@gmail.com>
2023-05-25 17:35:14 -0300

    chore: Component header and styling creation

---
+++ frontend/src/components/Button/index.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types'
+import './styles.scss'
+
+const Button = ({ children, className, onClick, as: Component }) => {
+  return (
+    <Component className={`button ${className}`} onClick={onClick}>
+      {children}
+    </Component>
+  )
+}
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+  onClick: PropTypes.func,
+  as: PropTypes.string,
+}
+
+Button.defaultProps = {
+  as: 'button',
+}
+
+export default Button
